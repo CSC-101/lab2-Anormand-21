e6ec520d51575e3e9cb357797ebd6fb0fb278eb7
Lab 2_1_Amanda Normand(4.9.26) <anormand@calpoly.edu>
2026-04-16 14:44:47 -0700

    officialtask4c

---
+++ task4c.py
@@ -0,0 +1,13 @@
+def surprising(L:list[str], other:str) -> list[str]:
+    L.append(other.upper())
+    return L
+
+
+words = ["this", "is", "confusing", "code."]
+first = surprising(words, "Avoid")
+second = surprising(words, "such.")
+         # What is the value of words at this point? ["this", "is", "confusing", "code.", "AVOID", "SUCH."]
+         # What are the values of first and second at this point? first  = ["this", "is", "confusing", "code.", "AVOID", "SUCH."]
+# second = ["this", "is", "confusing", "code.", "AVOID", "SUCH."]
+         # What happened? the function modified the original list using append
+print()
